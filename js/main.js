721f01ddc4af81116248cd419328845c72acfc75
41ahmad <111183178+41ahmad@users.noreply.github.com>
2023-09-24 09:19:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,14 +21,14 @@ let object;
 let controls;
 
 //Set which object to render
-let objToRender = 'textures';
+let objToRender = 'solar_farm_array';
 
 //Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
 
 //Load the file
 loader.load(
-  `solar_farm_array/textures/${objToRender}/scene.gltf`,
+  `solar_farm_array/${objToRender}/scene.gltf`,
   function (gltf) {
     //If the file is loaded, add it to the scene
     object = gltf.scene;
@@ -74,7 +74,7 @@ function animate() {
   //Here we could add some code to update the scene, adding some automatic movement
 
   //Make the eye move
-  if (object && objToRender === "solar_farm_array/textures") {
+  if (object && objToRender === "solar_farm_array") {
     //I've played with the constants here until it looked good 
     object.rotation.y = -3 + mouseX / window.innerWidth * 3;
     object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
